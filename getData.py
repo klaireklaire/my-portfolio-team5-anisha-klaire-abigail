233bea87a20e10a4d139e455bce785685afbddc2
Anisha <bhattacharyaanisha2001@gmail.com>
2024-07-29 15:17:42 -0400

    made data frame

--- getData.py
+++ getData.py
@@ -1,41 +1,34 @@
 import yfinance as yf
+import pandas as pd
 
 # List of ticker symbols
 ticker_symbols = ["AAPL", "AMZN", "TSLA", "META", "MSFT"]
 
-# List to store current prices
-current_prices = []
-price_changes = []
-price_changes_percentages = []
-
+stock_data=[]
 
 # Fetch the current price for each ticker
 for ticker in ticker_symbols:
     ticker_data = yf.Ticker(ticker)
 
     current_price_stock = ticker_data.info['currentPrice']
-    current_prices.append(current_price_stock)
     price_change_stock = ticker_data.info['currentPrice'] - ticker_data.info['previousClose']
-    price_changes.append(price_change_stock)
-    price_changes_percentage_stock = ticker_data.info['currentPrice']/ticker_data.info['previousClose']-1
-    price_changes_percentages.append(price_changes_percentage_stock)
+    price_change_percentage_stock = ticker_data.info['currentPrice']/ticker_data.info['previousClose']-1
+    fifty_two_week_low = ticker_data.info['fiftyTwoWeekLow']
+    fifty_two_week_high = ticker_data.info['fiftyTwoWeekHigh']
+    stock_data.append({
+        'Stock Ticker': ticker,
+        'Company Name': ticker_data.info['shortName'],
+        'Current Price': current_price_stock,
+        'Change in Price': price_change_stock,
+        'Change in Price %': price_change_percentage_stock,
+        '52-Week Low': fifty_two_week_low,
+        '52-Week High': fifty_two_week_high
+    }    
+    )
 
+print(stock_data)
 
-# Print the prices in order
-for ticker, price in zip(ticker_symbols, current_prices):
-    print(f"The current price of {ticker} is ${price:.2f}")
+columns = ['Stock Ticker', 'Company Name', 'Current Price', 'Change in Price', 'Change in Price %', '52-Week Low', '52-Week High']
 
-# Print the change in price in order
-for ticker, change in zip(ticker_symbols, price_changes):
-    print(f"The change in price of {ticker} is ${change:.2f}")
-
-# Print the change percentage in order
-for ticker, change_percent in zip(ticker_symbols, price_changes_percentages):
-    print(f"The change in price percentage of {ticker} is {change_percent:.2f}%")
-
-
-
-#thinking of what other data points are useful to us other than current prices
-#Price Change: The change in price from the previous close, both in absolute terms 
-# and as a percentage.
-
+portfolio_df = pd.DataFrame(stock_data, columns=columns)
+print(portfolio_df)
