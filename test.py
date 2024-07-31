140befa8204a7bc9419f39d5b47a0ef8a465d450
Anisha <bhattacharyaanisha2001@gmail.com>
2024-07-31 14:56:29 -0400

    merge conflict resolve

--- test.py
+++ test.py
@@ -15,12 +15,10 @@ df = pd.read_csv(f"https://query1.finance.yahoo.com/v7/finance/download/{stock}?
 
 print(df)
 
-<<<<<<< HEAD
 import yfinance as yf
 
 msft = yf.Ticker("MSFT")
 
 # get all stock info
 print(msft.info)
-=======
->>>>>>> AnishaBranch
+
