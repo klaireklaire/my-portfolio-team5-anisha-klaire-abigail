f77202789af5f5db50744c3f33494f0372505d3e
Anisha Bhattacharya <bhattacharyaanisha2001@gmail.com>
2024-08-02 17:40:35 +0000

    working on portfolio table

--- data_service/initialize_db.py
+++ data_service/initialize_db.py
@@ -2,3 +2,21 @@
 
 """initialize_db.py: Initialize MySQL database and connect it with Python and Flask app."""
 
+import sqlite3
+
+conn = sqlite3.connect("myPortfolio.sqlite")
+
+cursor = conn.cursor()
+sql_query="""   CREATE TABLE myPortfolio(
+    stock_ticker TEXT NOT NULL,
+    company_name TEXT NOT NULL,
+    current_price REAL NOT NULL,
+    change_in_price REAL,
+    change_in_price_percent REAL,
+    fifty_two_week_low REAL,
+    fifty_two_week_high REAL,
+    date_added TEXT,
+    number_of_shares INTEGER,
+    total_value REAL
+    PRIMARY KEY(stock_ticker)
+)"""
